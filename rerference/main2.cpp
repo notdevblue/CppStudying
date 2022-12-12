09be4073dff5681c1f3e6dc6878060af580e126c
HanUseWindows <wooyeophan@gmail.com>
2022-12-12 22:00:48 +0900

    12월 12일

--- rerference/main2.cpp
+++ rerference/main2.cpp
@@ -1,10 +1,19 @@
 ﻿#include <stdio.h>
 
-void Swap(int i, int j)
+//void Swap(int i, int j)
+//{
+//	int t = i;
+//	i = j;
+//	j = t;
+//}
+
+void Swap(int* i, int* j)
+// 이런 파라미터를 in out 파라미터라고 부름.
+// in으로 무언가를 주고, out 으로 파라미터 값이 바뀌어서 넘어오기 때문
 {
-	int t = i;
-	i = j;
-	j = t;
+	int t = *i;
+	*i = *j;
+	*j = t;
 }
 
 
@@ -24,9 +33,15 @@ int main()
 	//i = j;
 	//j = t;
 
-	Swap(i, j);
+	//Swap(i, j);
 	// 위와 다르게 안 바뀜
 	// 
+	// i, j 를 전달할 때
+	// i, j 값 말고
+	// 주소값을 전달해서 저장
+	// 그렇게 되면 원레 main 에 있는 i, j 값 바꿀 수 있음 
+
+	Swap(&i, &j);
 
 
 	printf("%i, %i\r\n", i, j);
